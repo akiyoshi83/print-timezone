b53f8ce93761b12adda78f9bf207931c2ef0f41b
akiyoshi83 <mail@akiyoshi.info>
2017-01-15 14:57:44 +0900

    Support multiple input time format

--- main.go
+++ main.go
@@ -8,11 +8,21 @@ import (
 )
 
 const (
-	// InputFormat is format for input time
-	InputFormat = "2006-01-02 15:04 MST"
 	// OutputFormat is format for output time
 	OutputFormat = "2006-01-02 15:04 MST -0700"
 )
+
+// InputFormats are to parse input time string
+var InputFormats = []string{
+	"2006-01-02 15:04 MST",
+	"2006/01/02 15:04 MST",
+	"2006-01-02 15:04:05 MST",
+	"2006/01/02 15:04:05 MST",
+	"MST 2006-01-02 15:04",
+	"MST 2006/01/02 15:04",
+	"MST 2006-01-02 15:04:05",
+	"MST 2006/01/02 15:04:05",
+}
 
 var defaultLocations = []string{
 	"UTC",
@@ -25,18 +35,36 @@ var defaultLocations = []string{
 func main() {
 	var src string
 	if len(os.Args) < 2 {
-		src = time.Now().Format(InputFormat)
+		src = time.Now().Format(InputFormats[0])
 	} else {
 		src = strings.Join(os.Args[1:], " ")
 	}
 
-	t, err := time.Parse(InputFormat, src)
+	t, err := tryParseTime(InputFormats, src)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "parse error %v", src)
+		fmt.Fprintf(os.Stderr, "parse error %v\n", src)
+		fmt.Fprintf(os.Stderr, "\n---SUPPORTED FORMATS---\n%v\n\n", strings.Join(InputFormats, "\n"))
 		os.Exit(1)
 	}
 
 	printWithTimezone(t, defaultLocations)
+}
+
+func tryParseTime(formats []string, s string) (time.Time, error) {
+	var parsed time.Time
+	var err error
+	for _, format := range formats {
+		var t time.Time
+		t, err = time.Parse(format, s)
+		if err == nil {
+			parsed = t
+			break
+		}
+	}
+	if err != nil {
+		return parsed, err
+	}
+	return parsed, nil
 }
 
 func formatWithTimezone(t time.Time, loc string) (string, error) {
