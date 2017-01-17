b16c208304d594b8233d4d8484f7bad98f7a8fd0
akiyoshi83 <mail@akiyoshi.info>
2017-01-17 23:25:35 +0900

    Divide package

--- main.go
+++ main.go
@@ -10,72 +10,41 @@ import (
 	"strings"
 	"time"
 
+	"github.com/akiyoshi83/print-timezone/ptz"
 	"gopkg.in/yaml.v2"
 )
-
-// PtzConf is application configuration
-type PtzConf struct {
-	Locations []string
-}
-
-const (
-	// ConfFileName is application configuration file name
-	ConfFileName = ".print-timezone.yml"
-	// OutputFormat is format for output time
-	OutputFormat = "2006-01-02 15:04 MST -0700"
-)
-
-// InputFormats are to parse input time string
-var InputFormats = []string{
-	"2006-01-02 15:04 MST",
-	"2006/01/02 15:04 MST",
-	"2006-01-02 15:04:05 MST",
-	"2006/01/02 15:04:05 MST",
-	"MST 2006-01-02 15:04",
-	"MST 2006/01/02 15:04",
-	"MST 2006-01-02 15:04:05",
-	"MST 2006/01/02 15:04:05",
-}
-
-var defaultLocations = []string{
-	"UTC",
-	"America/Los_Angeles",
-	"Europe/London",
-	"Asia/Tokyo",
-	"Australia/Sydney",
-}
 
 var (
 	defaultConfPath string
 	confPath        string
 	inputTime       string
-	conf            PtzConf
+	conf            ptz.Conf
 )
 
 func init() {
-	defaultConfPath = filepath.Join(homeDir(), ConfFileName)
-	conf.Locations = append(conf.Locations, defaultLocations...)
+	defaultConfPath = filepath.Join(homeDir(), ptz.ConfFileName)
+	conf.Locations = append(conf.Locations, ptz.DefaultLocations...)
 }
 
 func main() {
 	parseArgs()
 	loadConfig(confPath)
 
-	t, err := tryParseTime(InputFormats, inputTime)
+	t, err := ptz.TryParseTime(ptz.InputFormats, inputTime)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "parse error %v\n", inputTime)
-		fmt.Fprintf(os.Stderr, "\n---SUPPORTED FORMATS---\n%v\n\n", strings.Join(InputFormats, "\n"))
+		fmt.Fprintf(os.Stderr, "\n---SUPPORTED FORMATS---\n%v\n\n", strings.Join(ptz.InputFormats, "\n"))
 		os.Exit(1)
 	}
 
-	printWithTimezone(t, conf.Locations)
+	ptz.PrintWithTimezone(t, conf.Locations)
 }
 
 func parseArgs() {
 	flag.StringVar(&confPath, "f", "", "configuration file path")
 	flag.Parse()
 	if flag.NArg() < 2 {
-		inputTime = time.Now().Format(InputFormats[0])
+		inputTime = time.Now().Format(ptz.InputFormats[0])
 	} else {
 		inputTime = strings.Join(flag.Args()[:], " ")
 	}
@@ -117,43 +86,7 @@ func loadConfig(confPath string) error {
 
 	yaml.Unmarshal(data, &conf)
 	if len(conf.Locations) == 0 {
-		conf.Locations = append(conf.Locations, defaultLocations...)
+		conf.Locations = append(conf.Locations, ptz.DefaultLocations...)
 	}
 	return nil
-}
-
-func tryParseTime(formats []string, s string) (time.Time, error) {
-	var parsed time.Time
-	var err error
-	for _, format := range formats {
-		var t time.Time
-		t, err = time.Parse(format, s)
-		if err == nil {
-			parsed = t
-			break
-		}
-	}
-	if err != nil {
-		return parsed, err
-	}
-	return parsed, nil
-}
-
-func formatWithTimezone(t time.Time, loc string) (string, error) {
-	l, err := time.LoadLocation(loc)
-	if err != nil {
-		return "", fmt.Errorf("formatWithTimezone %v", err)
-	}
-	lt := t.In(l)
-	return lt.Format(OutputFormat), nil
-}
-
-func printWithTimezone(t time.Time, locations []string) {
-	for _, loc := range locations {
-		str, err := formatWithTimezone(t, loc)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "%v", err)
-		}
-		fmt.Printf("%s\n", str)
-	}
 }
---
+++ ptz/ptz.go
@@ -0,0 +1,79 @@
+package ptz
+
+import (
+	"fmt"
+	"os"
+	"time"
+)
+
+// Conf is application configuration
+type Conf struct {
+	Locations []string
+}
+
+const (
+	// ConfFileName is application configuration file name
+	ConfFileName = ".print-timezone.yml"
+	// OutputFormat is format for output time
+	OutputFormat = "2006-01-02 15:04 MST -0700"
+)
+
+// InputFormats are to parse input time string
+var InputFormats = []string{
+	"2006-01-02 15:04 MST",
+	"2006/01/02 15:04 MST",
+	"2006-01-02 15:04:05 MST",
+	"2006/01/02 15:04:05 MST",
+	"MST 2006-01-02 15:04",
+	"MST 2006/01/02 15:04",
+	"MST 2006-01-02 15:04:05",
+	"MST 2006/01/02 15:04:05",
+}
+
+// DefaultLocations is default supported timezone locations
+var DefaultLocations = []string{
+	"UTC",
+	"America/Los_Angeles",
+	"Europe/London",
+	"Asia/Tokyo",
+	"Australia/Sydney",
+}
+
+// TryParseTime parse `s` by `formats`
+func TryParseTime(formats []string, s string) (time.Time, error) {
+	var parsed time.Time
+	var err error
+	for _, format := range formats {
+		var t time.Time
+		t, err = time.Parse(format, s)
+		if err == nil {
+			parsed = t
+			break
+		}
+	}
+	if err != nil {
+		return parsed, err
+	}
+	return parsed, nil
+}
+
+// FormatWithTimezone format `t` in `loc` and return formated string
+func FormatWithTimezone(t time.Time, loc string) (string, error) {
+	l, err := time.LoadLocation(loc)
+	if err != nil {
+		return "", fmt.Errorf("FormatWithTimezone %v", err)
+	}
+	lt := t.In(l)
+	return lt.Format(OutputFormat), nil
+}
+
+// PrintWithTimezone write formated string to stdout
+func PrintWithTimezone(t time.Time, locations []string) {
+	for _, loc := range locations {
+		str, err := FormatWithTimezone(t, loc)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%v", err)
+		}
+		fmt.Printf("%s\n", str)
+	}
+}
